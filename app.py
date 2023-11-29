90371f54cb83540c5eb310c969e1f5573799f750
necynogueira <143413692+necynogueira@users.noreply.github.com>
2023-11-29 18:19:58 +0000

    Adicionando  estilos via CSS

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-  return "<p>Hello word!</p>"
+  return render_template("home.html")
 
 
 if __name__ == "__main__":
